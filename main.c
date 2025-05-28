50d85b4217dc2c2ce164c2c7b646cb615ff5db24
Enzo Cerneviva <contcerneviva@gmail.com>
2025-05-28 00:41:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,3 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+
+struct Cadastro{
+    char nome[100];
+    char telefone[100];
+    char documento[100];
+    char email[100];
+    float localizacao_lat;
+    float localizacao_lon;
+};
+
+struct Relato{
+  char catastrofe[50];
+  char descricao[201];
+  char data[20];
+  int hora;
+  float lat;
+  float lon;
+};
