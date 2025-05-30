638b0aac1f5ef681b1ef0b41b7e1178f576eff43
Enzo Cerneviva <contcerneviva@gmail.com>
2025-05-30 12:29:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
-#define RESET   "\033[0m"    // Volta ao estilo padrão
-#define RED     "\033[31m"   // Vermelho
-#define GREEN   "\033[32m"   // Verde
-#define YELLOW  "\033[33m"   // Amarelo
-#define BLUE    "\033[34m"   // Azul
-#define MAGENTA "\033[35m"   // Magenta
-#define CYAN    "\033[36m"   // Ciano
-#define BOLD    "\033[1m"    // Negrito
-#define CLEAR   "clear"      // Comando para limpar a tela (use "cls" se estiver no Windows)
+#include <ctype.h>
 
 // Constantes para o cálculo de distância com Haversine
 #define EARTH_RADIUS_KM 6371.0
@@ -37,10 +28,41 @@ struct Cadastro {
   double localizacao_lon;
 };
 
-void validacao_loc(char *latitude, char *longitude){
+// Função que verifica cada dígito para validar o formato de telefone inserido pelo usuário
+int validacao_telefone(char* telefone){
+
+  if(strlen(telefone) != 13) return 0; // verificação de 13 elementos
+
+  // isdigit ---> verifica se o char é um número ou não
+  if(!isdigit(telefone[0]) || !isdigit(telefone[1])) return 0;
+  if(telefone[2] != ' ') return 0;
+  for(int i = 3; i <= 7; i++){
+    if(!isdigit(telefone[i])) return 0;
+  }
+  if(telefone[8] != '-') return 0;
+  for(int i = 9; i <= 12; i++){
+    if(!isdigit(telefone[i])) return 0;
+  }
+
+  return 1;
 }
 
-void validacao_email(){}
+// Função que verifica se o email contém ao menos um '@' e um '.', garantindo um formato básico válido
+int validacao_email(char* email){
+  // strchr ---> procura o char escolhido dentro da string e se não achar retorna NULL
+  return (strchr(email, '@') && strchr(email, '.')); 
+}
+
+// Função que verifica a localização com limite de latitude e longitude
+int validacao_loc(double latitude, double longitude){
+  if(latitude > 90 || latitude < -90){
+    return 1;
+  }
+  else if(longitude > 180 || longitude < -180){
+    return 1;
+  }
+  return 0;
+}
 
 // Função para cadastrar um relator no sistema
 // Utiliza ponteiro para estrutura e manipulação de índice por referência
@@ -49,27 +71,65 @@ void cadastro_relator(struct Cadastro *relatores, int *n) {
 
   *n -= 1; // Ajuste temporário para usar o índice correto do vetor (trabalha com n-1)
 
+  // Leitura de nome
+  do{ 
   printf("Digite seu nome completo: ");
   fgets(relatores[*n].nome, 100, stdin);
   relatores[*n].nome[strcspn(relatores[*n].nome, "\n")] = '\0'; // Remove o '\n' do final
 
+  if(relatores[*n].nome[0] == '\0'){
+    printf("\nVocê não digitou seu nome! Tente novamente...\n\n");
+  }  
+
+  }while (relatores[*n].nome[0] == '\0');
+
+  // Leitura e validação de telefone
+  do{
+
   printf("Digite seu telefone (Ex: '11 99999-0000'): ");
   fgets(relatores[*n].telefone, 100, stdin);
+  relatores[*n].telefone[strcspn(relatores[*n].telefone, "\n")] = '\0'; 
 
+  if(!validacao_telefone(relatores[*n].telefone)){
+    printf("\n Esse número de telefone não é válido! Digite novamente...\n\n");
+  }  
+
+  }while(!validacao_telefone(relatores[*n].telefone));
+
+  // Leitura de documento
   printf("Digite seu documento (Ex: RG, CPF): ");
   fgets(relatores[*n].documento, 100, stdin);
+
+  // Leitura e validação do email
+  do{
 
   printf("Digite seu email: ");
   fgets(relatores[*n].email, 100, stdin);
 
-  // Leitura da localização geográfica do relator
-  printf("Digite sua localização - latitude (Ex:'-00.0000'): ");
+  if(!validacao_email(relatores[*n].email)){
+    printf("\n Esse formato não é um email! Digite novamente...\n\n");
+  }  
+
+  }while(!validacao_email(relatores[*n].email));
+
+  // Leitura da localização geográfica do relator com loop de validação
+  do{
+
+  printf("\n---- Insira sua localização em latitude e longitude ----\n\n");
+  printf("Latitude entre -90 e 90 (mín de 4 casas decimais, ex:'-00.0000'): ");
   scanf("%lf", &(relatores[*n].localizacao_lat));
   getchar();
 
-  printf("Digite sua localização - longitude (Ex:'-00.0000'): ");
+  printf("Longitude entre -180 e 180 (mín de 4 casas decimais, ex:'-00.0000'): ");
   scanf("%lf", &(relatores[*n].localizacao_lon));
   getchar();
+  
+  if(validacao_loc(relatores[*n].localizacao_lat, relatores[*n].localizacao_lon))
+  {
+    printf("\n Você digitou uma localização inexistente! Digite novamente...\n");
+  }
+  
+  } while(validacao_loc(relatores[*n].localizacao_lat, relatores[*n].localizacao_lon));
 
   *n += 2; // Corrige o índice para novos cadastros
 }
@@ -82,15 +142,15 @@ void cadastro_relatos(struct Relato *relatos, int *n) {
   *n -= 1; // Ajuste para o índice correto do vetor
 
   getchar();
-  printf("\nInsira o tipo de desastre natural ocorrido: ");
+  printf("\nInsira o tipo de desastre natural ocorrido......................: ");
   fgets(relatos[*n].catastrofe, 50, stdin);
   relatos[*n].catastrofe[strcspn(relatos[*n].catastrofe, "\n")] = '\0'; // Remove \n
 
-  printf("Insira uma descrição do desastre natural(até 200 caracteres): ");
+  printf("Insira uma descrição do desastre natural(até 200 caracteres)......: ");
   fgets(relatos[*n].descricao, 201, stdin);
   relatos[*n].descricao[strcspn(relatos[*n].descricao, "\n")] = '\0'; 
 
-  printf("Insira a data do desastre natural (DD/MM/AAAA): ");
+  printf("Insira a data do desastre natural (DD/MM/AAAA)....................: ");
   scanf("%s", relatos[*n].data);
   getchar();
 
@@ -98,13 +158,23 @@ void cadastro_relatos(struct Relato *relatos, int *n) {
   scanf("%s", relatos[*n].hora);
   getchar();
 
-  printf("Insira a localização do acontecimento (latitude): ");
-  scanf("%f", &(relatos[*n].lat));
-  getchar();
 
-  printf("Insira a localização do acontecimento (longitude): ");
-  scanf("%f", &(relatos[*n].lon));
-  getchar();
+  do{
+
+    printf("Insira a localização do acontecimento (latitude)................: ");
+    scanf("%f", &(relatos[*n].lat));
+    getchar();
+
+    printf("Insira a localização do acontecimento (longitude)...............: ");
+    scanf("%f", &(relatos[*n].lon));
+    getchar();
+
+      if(validacao_loc(relatos[*n].lat, relatos[*n].lon))
+    {
+      printf("\n Você digitou uma localização inexistente! Digite novamente...\n");
+    }
+  
+  } while(validacao_loc(relatos[*n].lat, relatos[*n].lon));
 
   *n += 2; // Corrige o índice novamente
 }
@@ -112,10 +182,10 @@ void cadastro_relatos(struct Relato *relatos, int *n) {
 // Função para exibir os dados do relator
 void exibir_relator(struct Cadastro *relatores) {
   printf("\n%s\n\n", relatores->nome);
-  printf("Telefone............: %s", relatores->telefone);
+  printf("Telefone............: %s\n", relatores->telefone);
   printf("Documento...........: %s", relatores->documento);
   printf("Email...............: %s", relatores->email);
-  printf("Latitude e Longitude: %.2f, %.2f\n", relatores->localizacao_lat, relatores->localizacao_lon);
+  printf("Latitude e Longitude: %.4f, %.4f\n", relatores->localizacao_lat, relatores->localizacao_lon);
 }
 
 // Função para exibir um relato específico
@@ -172,21 +242,23 @@ int busca_por_data(char *data1, char *data2){
 // Menu principal do programa
 void menu(int *opcao) {
   
-  printf(BLUE "\n=======================" RESET);
-  printf(BOLD " GEORELATO - MENU " RESET);
-  printf(BLUE "=======================\n\n" RESET);
+  printf( "\n=======================" );
+  printf( " GEORELATO - MENU " );
+  printf( "=======================\n\n" );
   
-  printf(GREEN " 1 " RESET "- Cadastrar Relato\n");
-  printf(GREEN " 2 " RESET "- Listar Todos os Relatos\n");
-  printf(GREEN " 3 " RESET "- Buscar por Tipo de Desastre Natural\n");
-  printf(GREEN " 4 " RESET "- Buscar por Localização (até 10 km de você)\n");
-  printf(GREEN " 5 " RESET "- Buscar por Período\n");
-  printf(GREEN " 6 " RESET "- Sair\n\n");
+  printf( " 1 - Cadastrar Relato\n");
+  printf( " 2 - Listar Todos os Relatos\n");
+  printf( " 3 - Buscar por Tipo de Desastre Natural\n");
+  printf( " 4 - Buscar por Localização (até 10 km de você)\n");
+  printf( " 5 - Buscar por Período\n");
+  printf( " 6 - Sair\n\n");
 
-  printf(BLUE"======================================================================================================\n" RESET);
+  printf("======================================================================================================\n" );
 
-  printf(YELLOW "Escolha uma das opções: " RESET);
+  printf("Escolha uma das opções: " );
   scanf("%d", opcao);
+
+  system("cls");
 }
 
 // Função principal - Ponto de entrada do programa
@@ -196,15 +268,17 @@ int main() {
   char data_busca[20];
   int n_relato = 1;                    // Controla quantos relatos foram cadastrados
   int n_relator = 1;                   // Controla os relatores cadastrados
-  int opcao;
+  int opcao;                           // Opção do Menu
+  int cont_loc = 0;                    // Contador de relatos próximos
+  int cont_data = 0;                    // Contador de relatos a partir de uma data
 
-  printf(BLUE"\n---------------------------"RESET BOLD" Bem vindo(a) ao GeoRelato!"BLUE"---------------------------\n\n"RESET); 
-  printf(BOLD"Esse é um sistema de cadastro e busca de relatos de desastres naturais com localização!\n"RESET);
-  printf(YELLOW BOLD"\nPara fazer e procurar relatos, por medidas de segurança, precisamos que você faça um cadastro em nosso sistema.\n"RESET);
+  printf("\n---------------------------" " Bem vindo(a) ao GeoRelato!""---------------------------\n\n"); 
+  printf("Esse é um sistema de cadastro e busca de relatos de desastres naturais com localização!\n");
+  printf("\nPara fazer e procurar relatos, por medidas de segurança, precisamos que você faça um cadastro em nosso sistema.\n");
 
   cadastro_relator(relatores, &n_relator); // Cadastro obrigatório do usuário ao iniciar o sistema
-  printf(GREEN"\nCadastro realizado com sucesso!\n" RESET);
-  printf(BOLD"\nInformações: \n"RESET);
+  printf("\nCadastro realizado com sucesso!\n" );
+  printf("\nInformações: \n");
   exibir_relator(relatores); // Exibe os dados do relator
 
   do {
@@ -245,10 +319,12 @@ int main() {
           }
         }
 
-        if (encontrados == 0)
+        if (encontrados == 0){
           printf("\nNenhum Desastre Natural do Tipo \"%s\" Foi Encontrado.\n", busca);
-        else
+        }
+        else{
           printf("\n%d Relato(s) do Tipo \"%s\" Encontrado(s).\n", encontrados, busca);
+        }
       break;
 
       case 4:
@@ -259,23 +335,29 @@ int main() {
           double distancia = haversine(relatores[0].localizacao_lat, relatores[0].localizacao_lon, relatos[i].lat, relatos[i].lon);
 
           if(distancia <= 10){
+            cont_loc += 1;
             exibir_relatos(&relatos[i], i + 1);
             printf("\nDistância de você em KM: %.2f \n", distancia);
           }
+
+          if(cont_loc == 0) printf("\n Nenhum relato foi encontrado próximo de você! \n");
         }
+
       break;
 
       case 5:
         // Busca relatos por data mínima
         printf("\n<--------------- BUSCA POR PERÍODO --------------->\n");
-        printf("\nEscolha uma data para buscar relatos cadastrados posteriormente: ");
+        printf("\nA partir de qual data você deseja visualizar os relatos cadastrados?(Ex: 00/00/0000): ");
         scanf("%s", data_busca);
-        printf("\nRelatos encontrados... \n");
 
         for(int i = 0; i < (n_relato - 1); i++){
           if(busca_por_data(relatos[i].data, data_busca) == 1){
+            cont_data += 1;
             exibir_relatos(&relatos[i], i + 1);
           }
+
+          if(cont_data == 0) printf("\n Nenhum relato foi encontrado após a data inserida! \n");
         }
       break;
 
